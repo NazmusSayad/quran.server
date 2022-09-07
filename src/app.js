5d72bb5876400a7bc74a5ce1da82f9a0e32d6eef
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-09-07 12:37:42 +0600

    smile :)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const cors = require('cors')
+const xss = require('xss-clean')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const mongoSanitize = require('express-mongo-sanitize')
+
 const response = require('./core/response')
 const router = require('./router')
-const app = express()
 
+const app = express()
+const globalLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 1000,
+  message: {
+    status: 'error',
+    message:
+      'Too many accounts created from this IP, please try again after an hour',
+  },
+})
+
+app.use(helmet())
 app.use(cors())
-app.use(express.json())
+app.use('*', globalLimiter)
+app.use(express.json({ limit: '12kb' }))
+app.use(mongoSanitize())
+app.use(xss())
 app.use(response.addSuccessMethod)
 
 app.use('/v1', router)
@@ -13,6 +32,3 @@ app.all('*', response.notFound)
 app.use(response.errorHandler)
 
 module.exports = app
-
-
-
