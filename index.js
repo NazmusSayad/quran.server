65ae0d5876b460f71d713d7e26a4fc5a764344dd
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-09-09 21:21:49 +0600

    ajaira node env

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-process.env.NODE_ENV ||= 'production'
+if (process.argv.at(-1) === '--NODE_ENV=development') {
+  console.clear()
+  process.env.NODE_ENV = 'development'
+  require('dotenv').config()
+} else process.env.NODE_ENV ||= 'production'
+
 require('./src/server')
